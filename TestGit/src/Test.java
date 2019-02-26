119611524f86115c48e50cfeb3066197934ed73d
liangjuan928 <47104709+liangjuan928@users.noreply.github.com>
2019-02-26 12:04:43 +0800

    Update Test.java
    娟在github修改了代码

--- TestGit/src/Test.java
+++ TestGit/src/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
          System.out.println("娟把项目的编码格式改成了utf-8");
          System.out.println("娟第一次提交");
+		 System.out.println("娟在github修改了代码");
 	}
 
 }
